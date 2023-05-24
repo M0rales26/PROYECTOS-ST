cf35c1c664a8fe3835999e1b9ed585af0c631eb2
M0rales26 <michael270918@outlook.com>
2023-05-23 21:35:21 -0500

    "Configurar Email"

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Notifications\CustomResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -35,7 +37,6 @@ class User extends Authenticatable{
      */
     protected $hidden = [
         'password',
-     
     ];
 
     /**
--- lang/es/passwords.php
+++ lang/es/passwords.php
@@ -14,9 +14,9 @@ return [
     */
 
     'reset' => '¡Su contraseña ha sido restablecida!',
-    'sent' => '¡Recordatorio de contraseña enviado al correo electrónico!',
+    'sent' => '¡Hemos enviado por correo electrónico el enlace para restablecer tu contraseña!',
     'token' => 'Este token de restablecimiento de contraseña es inválido.',
-    'user' => 'No se ha encontrado un usuario con esa dirección de correo.',
+    'user' => 'No podemos encontrar un usuario con esa dirección de correo electrónico.',
     'throttled' => 'Por favor espere antes de volver a intentarlo.',
     'password' => 'Las contraseñas deben tener al menos seis caracteres y coincidir con la confirmación.'
 ];
